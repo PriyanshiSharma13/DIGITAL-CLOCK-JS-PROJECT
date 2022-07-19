c0ee376c28021db442765ff2a4157d1437edad17
Priyanshi Sharma <priyanshisharma085@gmail.com>
2022-07-20 02:52:19 +0530

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 console.log("working");
 
+var wake = document.getElementById('slotOne');
+var lunch = document.getElementById('slotTwo');
+var nap = document.getElementById('slotThree');
+var night = document.getElementById('slotFour');    
+
+var groupOne = wake.options[wake.selectedIndex].value;
+var groupTwo = lunch.options[lunch.selectedIndex].value;
+var groupThree = wake.options[nap.selectedIndex].value;
+var groupFour = wake.options[night.selectedIndex].value;
+
+
 function showTime(){
     var showDisplayTime = new Date();
     var hours = showDisplayTime.getHours();
@@ -8,73 +19,68 @@ function showTime(){
     var session = document.getElementById("session");
     var hr = showDisplayTime.getHours();
 
-    if(hours >= 12){
-        session.innerHTML = 'PM';
-    }
-    else{
-        session.innerHTML = 'AM';
-    }
     if(hours > 12){
         hours = hours - 12;
     }
-    if((hours+"").length === 1){
-        hours = "0"+hours;
+    if(hours < 10){
+        "0" + showDisplayTime.getHours();
     }
-    if((mins+"").length === 1){
-        mins = "0"+mins;
+    if(mins < 10){
+        "0" + showDisplayTime.getMinutes();
     }
-    if((secs+"").length === 1){
-        secs = "0"+secs;
+    if(secs < 10){
+        "0" + showDisplayTime.getSeconds();
+    }
+    if(hours >= 12){
+        session.innerHTML = "PM";
+    }
+    else{
+        session.innerHTML = "AM";
     }
 
-    if((hr < 13 && hr > 7 )){
+    document.getElementById("hours").innerHTML = hours + `<br>hours`;
+    document.getElementById("mins").innerHTML = mins + `<br>mins`;
+    document.getElementById("secs").innerHTML = secs + `<br>secs`;
+
+    
+    if(groupOne == hr ){
         document.getElementById("changeImage").src = "goodmorning_image.jpg";
         document.getElementById("greetText").innerText = "GRAB SOME HEALTHY BREAKFAST!!!";
         document.getElementById("timeText").innerText = "GOOD MORNING!! WAKE UP !!";
     }
-    if((hr < 17 && hr > 12 )){
+    if(groupTwo == hr){
         document.getElementById("changeImage").src = "lunch_image.jpg";
         document.getElementById("greetText").innerText = "LET'S HAVE SOME LUNCH !!";
         document.getElementById("timeText").innerText = "GOOD AFTERNOON !! TAKE SOME SLEEP";
     }
-    if((hr < 21 && hr > 15 )){
+    if(groupThree == hr){
         document.getElementById("changeImage").src = "goodevening_image";
         document.getElementById("greetText").innerText = "STOP YAWNING, GET SOME TEA.. ITS JUST EVENING!";
-        document.getElementById("timeText").innerText = "GOOD EVENING !!";
+        document.getElementById("timeText").innerText = "GOOD EVENING !!";     
     }
-    if((hr < 25 && hr > 19 )){
+    if(groupFour == hr){
+        document.getElementById("changeImage").src = "goodnight_image.jpg";
         document.getElementById("greetText").innerText = "CLOSE YOUR EYES AND GO TO SLEEP";
         document.getElementById("timeText").innerText = "GOOD NIGHT !!";
-        document.getElementById("changeImage").src = "goodnight_image.jpg";
     }
-
-document.getElementById("hours").innerHTML = hours + `<br>hours`;
-document.getElementById("mins").innerHTML = mins + `<br>mins`;
-document.getElementById("secs").innerHTML = secs + `<br>secs`;
-
-
+    else{
+        document.getElementById("changeImage").src = "noddy_image.png";
+        document.getElementById("greetText").innerText = "My First Digital Clock";
+        document.getElementById("timeText").innerText = "Have a nice day :)";
+    }
 };
 setInterval(showTime,1000);
 
-let wake = document.getElementById('slotOne');
-let lunch = document.getElementById('slotTwo');
-let nap = document.getElementById('slotThree');
-let night = document.getElementById('slotFour');
-
 function givePower(){
+   
+    groupOne = wake.options[wake.selectedIndex].value;
+    groupTwo = lunch.options[lunch.selectedIndex].value;
+    groupThree = nap.options[nap.selectedIndex].value;
+    groupFour = night.options[night.selectedIndex].value;
+
     document.getElementById("wakeTime").innerText = wake.options[wake.selectedIndex].text;
     document.getElementById("lunchTime").innerText = lunch.options[lunch.selectedIndex].text;
     document.getElementById("napTime").innerText = nap.options[nap.selectedIndex].text;
     document.getElementById("nightTime").innerText = night.options[night.selectedIndex].text;
 
-    document.getElementById("greetText").innerText = "GRAB SOME HEALTHY BREAKFAST!!!";
-    document.getElementById("timeText").innerText = "GOOD MORNING!! WAKE UP !!";
-    document.getElementById("greetText").innerText = "LET'S HAVE SOME LUNCH !!";
-    document.getElementById("timeText").innerText = "GOOD AFTERNOON !! TAKE SOME SLEEP";
-    document.getElementById("greetText").innerText = "STOP YAWNING, GET SOME TEA.. ITS JUST EVENING!";
-    document.getElementById("timeText").innerText = "GOOD EVENING !!";
-    document.getElementById("timeText").innerText = "GOOD NIGHT !!";
-    document.getElementById("changeImage").src = "goodnight_image.jpg";
-}
-
-
+};
